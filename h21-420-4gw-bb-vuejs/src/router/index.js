e95e351648f4def1b085c8ba118f70d597baebb3
KikiTheScruffy <62897616+KikiTheScruffy@users.noreply.github.com>
2021-03-31 15:10:56 -0400

    ajout des components

--- h21-420-4gw-bb-vuejs/src/router/index.js
+++ h21-420-4gw-bb-vuejs/src/router/index.js
@@ -2,22 +2,22 @@ import {createRouter, createWebHistory} from 'vue-router'
 
 import Home from '../views/Home'
 import Add_Event from '../views/add-event'
-import Modify_Event from '../views/modify_event'
+import Change_Event from '../views/modify_event'
 const routes = [
     {
         path: '/',
         name: 'Home',
         component: Home
-    }
+    },
     {
         path: '/add-event',
         name: 'Add-event',
-        component: add-event
+        component: Add_Event
     },
     {
         path: '/change-event',
         name: 'Modifier évènement',
-        component: change-event
+        component: Change_Event
     },
 ]
 
